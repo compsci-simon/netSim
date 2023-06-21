1a861fba2976fd6efd680f5a29e8c7d82ab8cf3e
Simon <1simonsteven@gmail.com>
2023-06-21 19:01:46 +0200

    Added some fields and setters/getters for the fields

--- src/icmp.h
+++ src/icmp.h
@@ -32,6 +32,8 @@ class ICMP {
   unsigned char num_addrs {0};
   unsigned char addr_entry_size {0};
   short int lifetime {0};
+  short int identifier {0};
+  short int seq_num {0};
   // Router Address and Preference Level vector
   std::vector<int> rapl_vector;
 public:
@@ -40,11 +42,15 @@ public:
   void set_num_addrs(unsigned char num_addrs) { this-> num_addrs = num_addrs; }
   void set_addr_entry_size(unsigned char addr_entry_size) { this->addr_entry_size = addr_entry_size; }
   void set_lifetime(short int lifetime) { this->lifetime = lifetime; }
+  void set_identifier(short int identifier) { this->identifier = identifier; }
+  void set_seq_num(short int seq_num) { this->seq_num = seq_num; }
   unsigned char get_type() { return type; }
   unsigned char get_code() { return code; }
   unsigned char get_num_addrs() { return num_addrs; }
   unsigned char get_addr_entry_size() { return addr_entry_size; }
   short int get_lifetime() { return lifetime; }
+  short int get_identifier() { return identifier; }
+  short int get_seq_num() { return seq_num; }
   void get_byte_string(unsigned char* buffer);
   void instantiate_from_byte_string(unsigned char* buffer);
   void add_addr_and_pref(int addr, int pref);
